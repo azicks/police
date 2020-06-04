f19dfa7ae8c2d53a4ea8a0cb9d8405422785cb12
Dmitriy <34977658+azicks@users.noreply.github.com>
2020-06-04 12:53:12 +0700

    [6877#251507] Police project initialization

---
+++ src/main/java/police/configuration/RootConfig.java
@@ -0,0 +1,8 @@
+package police.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RootConfig implements WebMvcConfigurer {
+}
