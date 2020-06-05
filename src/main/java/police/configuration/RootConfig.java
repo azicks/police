d25ef8fb6cb7b319a845dfbb0e16ec8f7a4fa72c
Dmitriy <34977658+azicks@users.noreply.github.com>
2020-06-05 19:07:13 +0700

    [6877#251507] WebInit change

--- src/main/java/police/configuration/RootConfig.java
+++
@@ -1,8 +0,0 @@
-package police.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class RootConfig implements WebMvcConfigurer {
-}
